b80fb82719d149bd2f5336fbb9f76b773c13ecfb
Sree Duggirala <sreevanthduggirala@gmail.com>
2024-10-23 17:23:24 -0500

    Add sc boilerplate

--- contracts/src/Raffler.sol
+++ contracts/src/Raffler.sol
@@ -2,5 +2,34 @@
 pragma solidity ^0.8.13;
 
 contract Raffler {
-    
+    // Structure for storing all raffle information
+    struct Raffle {
+        uint8 x;
+        // Add more fields here
+    }
+
+    // Data structure/type to store all raffles
+    // Change variable visibility if needed
+
+    // Constructor to initialize Ownable() and VRF-related variables
+    constructor() {}
+
+    // Function to enter the raffle, adds participant to the raffle (by address)
+    // Change function visibility, args, and return value if needed
+    function enter() public {}
+
+    // Change function visibility, args, and return value if needed
+    function draw() public {}
+
+    // Function to request random number from VRF
+    // Change function visibility, args, and return value if needed
+    function requestRandomNumber() public {}
+
+    // Callback function for VRF to fulfill randomness and determine winner
+    // Change function visibility, args, and return value if needed
+    function fulfillRandomness() public {}
+
+    // Function to withdraw funds from the contract (for winner)
+    // Change function visibility, args, and return value if needed
+    function withdraw() public {}
 }
